01de75265a941674d015fc92af8a3d5d5da91486
pepinadas <mmmunapregunta@gmail.com>
2023-04-28 22:32:31 -0600

    Agregando la base del Back

---
+++ Back/jaz/src/main/java/com/ref/jaz/RefaccionariaApiApplication.java
@@ -0,0 +1,13 @@
+package com.ref.jaz;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RefaccionariaApiApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RefaccionariaApiApplication.class, args);
+	}
+
+}
