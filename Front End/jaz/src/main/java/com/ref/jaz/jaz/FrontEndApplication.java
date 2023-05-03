08e736860b668db3469c55f7dcbbf3959e5fd661
pepinadas <mmmunapregunta@gmail.com>
2023-05-03 00:10:18 -0600

    Llevo algunas cosas...

--- Front End/jaz/src/main/java/com/ref/jaz/jaz/FrontEndApplication.java
+++ Front End/jaz/src/main/java/com/ref/jaz/jaz/FrontEndApplication.java
@@ -2,12 +2,19 @@ package com.ref.jaz.jaz;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
 public class FrontEndApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(FrontEndApplication.class, args);
+	}
+
+	@Bean
+	public WebClient webClient(WebClient.Builder builder) {
+		return builder.build();
 	}
 
 }
--- Front End/jaz/src/main/java/com/ref/jaz/jaz/controlador/Principal.java
+++ Front End/jaz/src/main/java/com/ref/jaz/jaz/controlador/Principal.java
@@ -1,10 +1,30 @@
 package com.ref.jaz.jaz.controlador;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 @Controller
 public class Principal {
 
-    
+    private final WebClient webClient;
+
+    @Autowired
+    public Principal(WebClient webClient) {
+        this.webClient = webClient;
+    }
+
+    @GetMapping("/inicio")
+    public String getInicio(Model model){
+        String uri = "http:/localhost:8081/refaccion/api";
+        model.addAttribute("uri", uri);
+        return "index";
+    }
 
 }
+
+//        RestTemplate restConsumer = new RestTemplate();
+//
