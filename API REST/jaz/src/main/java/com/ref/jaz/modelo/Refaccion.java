719a1319cff6ebd60691200cd5e4c2704ea1c9d4
jazmin08ibieta <ibieta.jazmin8@gmail.com>
2023-06-14 03:13:19 -0600

    Back terminado2.0

--- API REST/jaz/src/main/java/com/ref/jaz/modelo/Refaccion.java
+++ API REST/jaz/src/main/java/com/ref/jaz/modelo/Refaccion.java
@@ -1,5 +1,6 @@
 package com.ref.jaz.modelo;
 
+import jakarta.persistence.*;
 import lombok.*;
 
 @AllArgsConstructor
@@ -7,14 +8,18 @@ import lombok.*;
 @Getter
 @Setter
 @ToString
+@Entity
+@Table(name = "refaccion")
 public class Refaccion {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
     private int id;
     private String imagen;
     private String modelo;
     private String descripcion;
-    private String marca; //Tal vez deberiamos pensar en declarar esto como un List<String> marca, para que sean valores establecidos y super a menos de que la marca fuera previamente creada, no sea valida, para evitar faltas de ortografia
-    private double costo;
-    private String categoria; //Lo mismo con esto
+    private String marca;
+    private int costo;
+    private String categoria;
     private int cantidad;
 //post vender
     public boolean vender(){
---
+++ API REST/jaz/src/main/java/com/ref/jaz/persistencia/RefaccionRepository.java
@@ -0,0 +1,11 @@
+package com.ref.jaz.persistencia;
+
+import com.ref.jaz.modelo.Refaccion;
+import org.springframework.data.repository.CrudRepository;
+
+public interface RefaccionRepository extends CrudRepository<Refaccion, Integer> {
+
+
+
+
+}
